bb4f78290823077bf760d4f461a16031552fc6ad
milena-mognon <milenamognon@gmail.com>
2019-06-08 09:55:37 -0300

    First Commit

---
+++ app/Http/Controllers/Admin/OrigensController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Origem;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrigensController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Origem  $origem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Origem $origem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Origem  $origem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Origem $origem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Origem  $origem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Origem $origem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Origem  $origem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Origem $origem)
+    {
+        //
+    }
+}
---
+++ resources/lang/pt-br/atributes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+
+    'user' => [
+        'users' => 'Usuários',
+        'user' => 'Usuário',
+        'name' => 'Nome',
+        'email' => 'Email',
+        'password' => 'Senha',
+        'job_role' => 'Cargo',
+        'section' => 'Seção',
+        'password_confirmation' => 'Confirmar Senha',
+        'new_password' => 'Nova Senha',
+        'current_password' => 'Senha Atual'
+    ],
+
+    'gun' => [],
+    'ammunition' => [],
+    'components' => [],
+    'expert_report' => [],
+];

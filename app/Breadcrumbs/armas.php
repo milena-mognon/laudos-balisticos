20ffe8f90d2b963ec63da10014e3b6ea4b2b7475
milena-mognon <milenamognon@gmail.com>
2019-09-03 11:02:43 -0300

    fixed breadcrumbs

--- app/Breadcrumbs/armas.php
+++ app/Breadcrumbs/armas.php
@@ -1,11 +1,21 @@
 <?php
 
-Breadcrumbs::register('armas.create', function ($breadcrumbs, $laudo) {
+Breadcrumbs::register('revolveres.create', function ($breadcrumbs, $laudo) {
     $breadcrumbs->parent('laudos.materiais', $laudo);
-    $breadcrumbs->push('Nova Arma', route('armas.create', $laudo));
+    $breadcrumbs->push('Novo Revólver', route('revolveres.create', $laudo));
 });
 
-Breadcrumbs::register('armas.edit', function ($breadcrumbs, $laudo, $arma) {
+Breadcrumbs::register('revolveres.edit', function ($breadcrumbs, $laudo, $arma) {
     $breadcrumbs->parent('laudos.show', $laudo);
-    $breadcrumbs->push("Editar $arma->tipo_arma" , route('armas.edit',[ $laudo, $arma]));
+    $breadcrumbs->push("Editar $arma->tipo_arma" , route('revolveres.edit',[ $laudo, $arma]));
+});
+
+Breadcrumbs::register('espingardas.create', function ($breadcrumbs, $laudo) {
+    $breadcrumbs->parent('laudos.materiais', $laudo);
+    $breadcrumbs->push('Nova Espingarda', route('espingardas.create', $laudo));
+});
+
+Breadcrumbs::register('espingardas.edit', function ($breadcrumbs, $laudo, $arma) {
+    $breadcrumbs->parent('laudos.show', $laudo);
+    $breadcrumbs->push("Editar $arma->tipo_arma" , route('espingardas.edit',[ $laudo, $arma]));
 });
